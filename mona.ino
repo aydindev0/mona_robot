1fd9118a853636f316835b3560593e8b829976fa
aydindev0 <92227214+aydindev0@users.noreply.github.com>
2025-06-18 07:12:20 +0100

    feat: starter code.

---
+++ mona.ino
@@ -0,0 +1,81 @@
+#include <Wire.h>
+#include "Mona_ESP_lib.h"
+#include <ESP32Encoder.h> 
+
+ESP32Encoder right_encoder;
+ESP32Encoder left_encoder;
+
+int right_new_pos, right_old_pos, left_new_pos, left_old_pos;
+float right_vel, left_vel, right_ref_vel, left_ref_vel;
+
+// set sample time
+float Ts = 0.01;
+
+float start_time, current_time, elapsed_time;
+
+
+
+// Wheel radius is 15mm, every revolute is 3500 pulse
+const float robot_radius = 40;
+const float wheel_radius = 15;
+
+// The post-gearbox resolution is 3500 pulse per wheel revolution and 
+// the radius of wheel is 15mm. In this case, the following scale_encoder
+// value should be set so that the measurement results show the linear displacement
+// in mm at the Serial Monitor/Plotter
+
+const float scale_encoder = 1;    
+const float pi = 3.14159; 
+
+
+
+
+
+void setup(){
+    Mona_ESP_init();
+    Serial.begin(115200);
+    right_encoder.attachHalfQuad ( Mot_right_feedback, Mot_right_feedback_2 );
+    left_encoder.attachHalfQuad( Mot_left_feedback_2, Mot_left_feedback );
+
+	// clear starting values
+	right_encoder.clearCount();
+	left_encoder.clearCount();
+
+	// set the lastToggle
+	current_time = millis();
+    start_time = current_time;
+
+    // set the initial positions
+
+    right_old_pos = 0;
+    left_old_pos = 0;
+    delay(5000);
+    Left_mot_forward(128);
+    Right_mot_forward(128);
+    }
+
+
+
+void position(){
+  elapsed_time = (millis()-current_time)/1000;    // elapsed time in seconds
+  right_new_pos = right_encoder.getCount();
+  left_new_pos = left_encoder.getCount();
+  right_old_pos = right_new_pos; 
+  left_old_pos = left_new_pos;
+}
+
+
+
+void loop(){
+  while (current_time-start_time<10000){
+    position();
+    Serial.print(scale_encoder*right_old_pos);
+    Serial.print("\t")
+    Serial.print(scale_encoder*left_old_pos);
+    Serial.println();
+    current_time = millis();
+    delay(Ts*1000);
+    }
+  Motors_stop();
+  Serial.end();
+}
